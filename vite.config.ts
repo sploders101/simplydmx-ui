8ae0b1f7c548967280a41d91cf33eed8c89cc26b
Shaun Keys <mariobuddy@gmail.com>
2022-08-27 13:35:40 -0600

    Configure vite for tauri

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import vue from "@vitejs/plugin-vue"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-})
+	clearScreen: false,
+	server: {
+		strictPort: true,
+	},
+	envPrefix: ["VITE_", "TAURI_"],
+	build: {
+		// Tauri supports es2021
+		target: ['es2021', 'chrome100', 'safari13'],
+		// don't minify for debug builds
+		minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+		// produce sourcemaps for debug builds
+		sourcemap: !!process.env.TAURI_DEBUG,
+	},
+});
