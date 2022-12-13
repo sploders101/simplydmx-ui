4b4204bcbd9c42c60575a54b16db3493aae6220a
Shaun Keys <mariobuddy@gmail.com>
2022-12-12 21:34:49 -0700

    Add largeselect to fixtures tab

--- src/views/patcher/FixturesTab.vue
+++ src/views/patcher/FixturesTab.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
 	import { computed, ref, watch } from "vue";
 	import { usePatcherState } from "@/stores/patcher";
+	import DynamicForm from "@/components/forms/DynamicForm.vue";
+	import {
+		patcher,
+	} from "@/scripts/api/ipc";
 
 	const state = usePatcherState();
 	const lightOptions = computed(() => {
@@ -15,13 +19,25 @@
 		});
 	});
 
+	const selectedFixture = ref<string | null>(null);
+
 	watch(lightOptions, () => {
 		if (lightOptions.value.length && !lightOptions.value.find((item) => item.value === selectedFixture.value)) {
 			selectedFixture.value = lightOptions.value[0].value;
 		}
 	}, { immediate: true });
 
-	const selectedFixture = ref<string | null>(null);
+	const addFixtureDialog = ref<boolean>(false);
+	const newFixtureData = ref<null>(null);
+	function cancelAddingFixture() {
+		addFixtureDialog.value = false;
+		newFixtureData.value = null;
+	}
+
+	/** Adds the fixture  */
+	function addFixture() {
+		return
+	}
 </script>
 
 <template>
@@ -34,12 +50,28 @@
 			>
 			<template #header-right>
 				<Tooltip text="Add Fixture">
-					<Button icon subtle><Icon i="plus"/></Button>
+					<Button @click="addFixtureDialog = true" icon subtle><Icon i="plus"/></Button>
 				</Tooltip>
 			</template>
 		</LargeSelect>
 		<div class="patcher-fixture-prefs">
 		</div>
+
+		<Dialog :visible="addFixtureDialog">
+			<template #header>
+				Add Fixture
+			</template>
+			
+			<template #footer>
+				<Button @click="cancelAddingFixture()" subtle class="spaced">
+					Cancel
+				</Button>
+				<div class="spacer"/>
+				<Button @click="addFixture()" class="spaced">
+					Ok
+				</Button>
+			</template>
+		</Dialog>
 	</div>
 </template>
 
--- src/views/patcher/patcher.vue
+++ src/views/patcher/patcher.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
-	import { computed } from "vue";
-	import { usePatcherState } from "@/stores/patcher";
 	import FixturesTab from "./FixturesTab.vue";
-
-	const state = usePatcherState();
-
-	const stringifiedData = computed(() => {
-		return JSON.stringify(state.value);
-	});
 </script>
 
 <template>
@@ -16,13 +8,6 @@
 			<FixturesTab/>
 		</Tabitem>
 		<Tabitem tab="universes">
-			<!--<LargeSelect :options="[1, 2, 3, 4, 5].map((i) => ({ label: 'Test ' + i, value: i}))" @select="log" enableSearch>
-				<template #header-right>
-					<Tooltip text="Add Fixture">
-						<Button icon subtle><Icon i="plus"/></Button>
-					</Tooltip>
-				</template>
-			</LargeSelect>-->
 		</Tabitem>
 	</Tabs>
 </template>
