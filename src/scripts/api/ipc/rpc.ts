284c97967902640822adfc1b25277a3b90ad8eda
Shaun Keys <mariobuddy@gmail.com>
2022-12-13 19:39:11 -0700

    Automate documentation for TS types

--- src/scripts/api/ipc/rpc.ts
+++ src/scripts/api/ipc/rpc.ts
@@ -1,13 +1,18 @@
 import { callService } from "./agnostic_abstractions";
 
 
-// AbstractLayerLight
+/**
+ * Represents the abstract data for a single light in a layer.
+ * A value's binary may be masked if the output is u8 (integer overflow cast)
+ */
 export type AbstractLayerLight = Record<string, BlenderValue>;
 
-// BlenderValue
+/**
+ * Value to be used in a submaster with instructions for mixing it into the result
+ */
 export type BlenderValue = { type: "None" } | { type: "Static"; value: number } | { type: "Offset"; value: number };
 
-// BlendingData
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface BlendingData {
     scheme: BlendingScheme;
     snap: SnapData;
@@ -16,87 +21,108 @@ export interface BlendingData {
     min_value: number;
 }
 
-// BlendingScheme
+/**
+ * The method in which conflicts are resolved while blending
+ */
 export type BlendingScheme = "HTP" | "LTP";
 
-// Channel
+/**
+ * Information about a specific channel available on the fixture
+ */
 export interface Channel {
     size: ChannelSize;
     default?: number;
     ch_type: ChannelType;
 }
 
-// ChannelSize
+/**
+ * Dictates the size of the output. Values will be stored as the largest of these options, but bounds
+ * will be enforced by the UI, mixer, and output will be truncated.
+ */
 export type ChannelSize = "U8" | "U16";
 
-// ChannelType
+/**
+ * Describes information used for controlling and blending the channel
+ */
 export type ChannelType = { type: "Segmented"; segments: Segment[]; priority: BlendingScheme; snapping: SnapData | null } | { type: "Linear"; priority: BlendingScheme };
 
-// CreateFixtureError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type CreateFixtureError = "FixtureTypeMissing" | "ControllerMissing" | { ErrorFromController: CreateInstanceError };
 
-// CreateInstanceError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type CreateInstanceError = { type: "InvalidData" } | ({ type: "Other" } & string) | { type: "Unknown" };
 
-// DMXFixtureData
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface DMXFixtureData {
     personalities: Record<string, DMXPersonalityData>;
 }
 
-// DMXFixtureInstance
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface DMXFixtureInstance {
     universe: Uuid | null;
     offset: number | null;
 }
 
-// DMXInitializationError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type DMXInitializationError = "UnrecognizedData";
 
-// DMXPersonalityData
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface DMXPersonalityData {
     dmx_channel_order: string[];
 }
 
-// DMXShowSave
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface DMXShowSave {
     library: Record<Uuid, DMXFixtureData>;
     fixtures: Record<Uuid, DMXFixtureInstance>;
     universes: Record<Uuid, UniverseInstance>;
 }
 
-// DisplayableDMXDriver
+/**
+ * Minified representation of a DMX driver for display
+ */
 export interface DisplayableDMXDriver {
     id: string;
     name: string;
     description: string;
 }
 
-// E131DMXShowSave
+/** Describes a value to be shown in a dropdown list */
+export interface DropdownOptionJSON {
+    name: string;
+    description: string | null;
+    value: Value;
+}
+
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface E131DMXShowSave {
     universes: Record<Uuid, E131Universe>;
 }
 
-// E131InitializationError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type E131InitializationError = "UnrecognizedData";
 
-// E131Universe
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface E131Universe {
     external_universe: number;
 }
 
-// EditError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type EditError = { type: "InvalidData" } | ({ type: "Other" } & string) | { type: "Unknown" };
 
-// FilterCriteria
+/** Represents criteria used to filter an event. For example, a submaster UUID could be used to filter submaster updates by that specific submaster */
 export type FilterCriteria = { type: "None" } | { type: "String"; data: string } | { type: "Uuid"; data: Uuid };
 
-// FixtureBundle
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface FixtureBundle {
     fixture_info: FixtureInfo;
     output_info: SerializedData;
 }
 
-// FixtureInfo
+/**
+ * Data type that contains generic, protocol-erased information about a fixture such as name,
+ * metadata, personalities, and references to services within the output controller.
+ */
 export interface FixtureInfo {
     id: Uuid;
     name: string;
@@ -109,7 +135,9 @@ export interface FixtureInfo {
     output_driver: string;
 }
 
-// FixtureInstance
+/**
+ * Identifies an individual instance of a fixture
+ */
 export interface FixtureInstance {
     id: Uuid;
     fixture_id: Uuid;
@@ -118,102 +146,117 @@ export interface FixtureInstance {
     comments: string | null;
 }
 
-// FixtureMeta
+/**
+ * Metadata about the fixture, used for display in the UI
+ */
 export interface FixtureMeta {
     manufacturer: string | null;
     manual_link: string | null;
 }
 
-// FormDescriptor
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type FormDescriptor = FormItem[];
 
-// FormDropdown
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface FormDropdown {
     label: string;
     id: string;
     item_source: FormItemOptionSource;
 }
 
-// FormItem
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type FormItem = { Textbox: FormTextbox } | { Number: FormNumber } | { Dropdown: FormDropdown } | { Section: FormSection } | { VerticalStack: FormItem[] } | { HorizontalStack: FormItem[] };
 
-// FormItemOptionSource
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type FormItemOptionSource = { Static: { values: DropdownOptionJSON[] } } | { TypeSpec: { typespec_id: string } };
 
-// FormNumber
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface FormNumber {
     label: string;
     id: string;
 }
 
-// FormSection
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface FormSection {
     label: string;
     form_items: FormItem[];
 }
 
-// FormTextbox
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface FormTextbox {
     label: string;
     id: string;
 }
 
-// ImportError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type ImportError = { type: "InvalidData" } | ({ type: "Other" } & string) | { type: "Unknown" };
 
-// ImportFixtureError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type ImportFixtureError = "UnknownController" | { ErrorFromController: ImportError };
 
-// JSONCallServiceError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type JSONCallServiceError = { type: "ServiceNotFound" } | { type: "ArgDeserializationFailed" } | { type: "ResponseSerializationFailed" };
 
-// JSONCommand
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type JSONCommand = { type: "CallService"; message_id: number; plugin_id: string; service_id: string; args: Value[] } | { type: "GetServices"; message_id: number } | { type: "GetOptions"; message_id: number; provider_id: string } | { type: "SendEvent"; name: string; criteria: FilterCriteria | null; data: Value } | { type: "Subscribe"; name: string; criteria: FilterCriteria | null } | { type: "Unsubscribe"; name: string; criteria: FilterCriteria | null };
 
-// JSONResponse
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type JSONResponse = { type: "CallServiceResponse"; message_id: number; result: Value } | { type: "ServiceList"; message_id: number; list: ServiceDescription[] } | { type: "OptionsList"; message_id: number; list: { Ok: DropdownOptionJSON[] } | { Err: TypeSpecifierRetrievalError } } | { type: "CallServiceError"; message_id: number; error: JSONCallServiceError } | { type: "Event"; name: string; criteria: FilterCriteria; data: Value };
 
-// LinkUniverseError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type LinkUniverseError = { type: "ErrorFromController"; data: RegisterUniverseError } | { type: "UniverseNotFound" } | { type: "ControllerNotFound" };
 
-// MixerContext
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface MixerContext {
     default_context: MixingContext;
     frozen_context: MixingContext | null;
     blind_opacity: number;
 }
 
-// MixerInitializationError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type MixerInitializationError = "UnrecognizedData";
 
-// MixingContext
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface MixingContext {
     layer_order: Uuid[];
     layer_opacities: Record<Uuid, number>;
     user_submasters: Record<Uuid, StaticLayer>;
 }
 
-// PatcherInitializationError
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type PatcherInitializationError = "UnrecognizedData";
 
-// Personality
+/**
+ * Identifies non-implementation-specific features of a personality.
+ * 
+ * Implementation-specific features of a personality such as channel order should
+ * should be stored in the output data for use by the output plugin.
+ */
 export interface Personality {
     available_channels: string[];
 }
 
-// RegisterSavableError
+/**
+ * An error returned when registering a saver. This is usually okay to unwrap, since it should be during init
+ */
 export type RegisterSavableError = { type: "SaverAlreadyExists" };
 
-// RegisterUniverseError
+/**
+ * An error that occurs while registering a universe
+ */
 export type RegisterUniverseError = "InvalidData" | { Other: string } | "Unknown";
 
-// SaveError
+/**
+ * An error returned by the saver if saving data failed
+ */
 export type SaveError = { type: "SaverReturnedErr"; data: { error: string } } | { type: "ErrorSerializing"; data: { error: string } } | { type: "Unsafe" };
 
-// SaverInitializationStatus
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type SaverInitializationStatus = { type: "FinishedSafe" } | { type: "FinishedUnsafe" } | { type: "Initializing" };
 
-// Segment
+/**
+ * Identifies a segment used in a segmented channel
+ */
 export interface Segment {
     start: number;
     end: number;
@@ -221,10 +264,14 @@ export interface Segment {
     id: string;
 }
 
-// SerializedData
+/**
+ * Data type used to hold a serialized instance of an arbitrary data type.
+ * 
+ * This is intended to encapsulate dynamically-typed data intended for deserialization by the output plugin
+ */
 export type SerializedData = number[] | Value;
 
-// ServiceArgumentOwned
+/** Describes an argument that must be passed to a service call */
 export interface ServiceArgumentOwned {
     id: string;
     name: string;
@@ -233,7 +280,7 @@ export interface ServiceArgumentOwned {
     val_type_hint: string | null;
 }
 
-// ServiceDescription
+/** Describes a service that can be called from an external API */
 export interface ServiceDescription {
     plugin_id: string;
     id: string;
@@ -243,40 +290,45 @@ export interface ServiceDescription {
     returns: ServiceArgumentOwned | null;
 }
 
-// SharablePatcherState
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface SharablePatcherState {
     library: Record<Uuid, FixtureInfo>;
     fixture_order: Uuid[];
     fixtures: Record<Uuid, FixtureInstance>;
 }
 
-// ShowFile
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface ShowFile {
     plugin_data: Record<string, number[]>;
 }
 
-// SnapData
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export type SnapData = { type: "NoSnap" } | { type: "SnapAt"; data: number };
 
-// StaticLayer
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface StaticLayer {
     values: SubmasterData;
 }
 
-// SubmasterData
+/**
+ * Represents the data within a submaster used for blending
+ */
 export type SubmasterData = Record<Uuid, AbstractLayerLight>;
 
-// UniverseInstance
+/** Describes an error that occurred while retrieving items for a dropdown list */
+export type TypeSpecifierRetrievalError = "SpecifierNotFound";
+
+/** This type is currently undocumented. I will be working to resolve this for all types in the near future. */
 export interface UniverseInstance {
     id: Uuid;
     name: string;
     controller: string | null;
 }
 
-// Uuid
+/** Unique identifier used in various parts of the API. In TS, UUID does not have its own data type, so this just re-exports string. */
 export type Uuid = string;
 
-// Value
+/** Represents Rust's `serde_json::Value` type. This is used for dynamic typing, like when using backend-defined forms. */
 export type Value = any;
 
 
