fdc5f2c614fddcaf402a13db2b8cf06e5d9d68d9
Shaun Keys <mariobuddy@gmail.com>
2023-01-18 23:15:26 -0700

    Split the fixture creation dialog into a component

--- src/views/patcher/FixturesTab.vue
+++ src/views/patcher/FixturesTab.vue
@@ -1,27 +1,9 @@
 <script lang="ts" setup>
-	import { computed, ref, watch } from "vue";
+	import { computed, ref } from "vue";
 	import { usePatcherState } from "@/stores/patcher";
-	import DynamicForm from "@/components/forms/DynamicForm.vue";
-	import {
-		exhaustiveMatch,
-		FormDescriptor,
-		patcher,
-	} from "@/scripts/api/ipc";
-	import { Option } from "@/components/generic/dropdown.vue";
-	import { unwrap } from "@/scripts/helpers";
-	import { getDefaultFormData } from "@/components/forms/helpers";
+	import CreateFixtureDialog from "./CreateFixtureDialog.vue";
 
 	const state = usePatcherState();
-	const fixtureTypes = computed<Option[]>(() => {
-		if (!state.value) return [];
-
-		return Object.values(state.value.library).map((info) => {
-			return {
-				name: info.name,
-				value: info.id,
-			};
-		});
-	});
 	const lightOptions = computed(() => {
 		if (!state.value) return [];
 		return state.value.fixture_order.map((value) => {
@@ -32,94 +14,9 @@
 			};
 		});
 	});
-	const personalityOptions = computed(() => {
-		if (!state.value) return [];
-		if (!fixtureToAdd.value) return [];
-		return Object.keys(state.value.library[fixtureToAdd.value].personalities).map<Option>((key) => ({
-			name: key,
-			value: key,
-		}));
-	})
 
-	const selectedFixture = ref<string | null>(null);
-
-	watch(lightOptions, () => {
-		if (lightOptions.value.length && !lightOptions.value.find((item) => item.value === selectedFixture.value)) {
-			selectedFixture.value = lightOptions.value[0].value;
-		}
-	}, { immediate: true });
-
-	const addFixtureDialog = ref<boolean>(false);
-	function cancelAddingFixture() {
-		addFixtureDialog.value = false;
-		fixtureToAdd.value = null;
-		personality.value = null;
-		name.value = "";
-	}
-
-	const fixtureToAdd = ref<string | null>(null);
-	const personality = ref<string | null>(null);
-	const name = ref<string>("");
-
-	const addFixtureForm = ref<[FormDescriptor, any] | null>(null);
-	watch(fixtureToAdd, (queryFixture) => {
-		if (queryFixture) {
-			patcher.get_creation_form(queryFixture)
-				.then((formResult) => {
-					if (fixtureToAdd.value === queryFixture) {
-						let formDescriptor = unwrap(formResult);
-						let formData = getDefaultFormData(formDescriptor);
-						addFixtureForm.value = [formDescriptor, formData];
-					}
-				});
-		} else {
-			addFixtureForm.value = null;
-		}
-	});
-
-	const creationError = ref<{ header: string, body: string } | null>(null);
-
-	/** Adds the fixture  */
-	async function addFixture() {
-		if (fixtureToAdd.value && personality.value && name.value && addFixtureForm.value) {
-			let response = await patcher.create_fixture(
-				fixtureToAdd.value,
-				personality.value,
-				name.value,
-				"",
-				addFixtureForm.value[1],
-			);
-			exhaustiveMatch(response, {
-				"Ok": () => cancelAddingFixture(),
-				"Err": (err) => exhaustiveMatch(err, {
-					"ControllerMissing": () => creationError.value = {
-						header: "An error occurred",
-						body: "SimplyDMX does not know how to control this fixture. Please make sure any relevant plugins have been loaded.",
-					},
-					"FixtureTypeMissing": () => creationError.value = {
-						header: "An error occurred",
-						body: "SimplyDMX does not recognize the requested fixture. Please file a bug report with the author.",
-					},
-					"ErrorFromController": (err) => exhaustiveMatch(err, {
-						"InvalidData": () => creationError.value = {
-							header: "Form validation failed",
-							body: "SimplyDMX failed to convert form data into a valid fixture definition.",
-						},
-						"Other": (description) => creationError.value = {
-							header: "Something went wrong",
-							body: description,
-						},
-						"Unknown": () => creationError.value = {
-							header: "An error occurred",
-							body: "Unknown error",
-						},
-					}),
-				}),
-			});
-		} else {
-			alert("Invalid form details");
-		}
-	}
+	const selectedFixture = ref(null);
+	const addFixtureDialog = ref(false);
 </script>
 
 <template>
@@ -138,64 +35,7 @@
 		</LargeSelect>
 		<div class="patcher-fixture-prefs">
 		</div>
-
-		<Dialog :visible="addFixtureDialog" :show-close="false">
-			<template #header>
-				Add Fixture
-			</template>
-
-			<Dropdown
-				label="Fixture Type"
-				v-model="fixtureToAdd"
-				:options="fixtureTypes"
-				class="spaced"
-				/>
-
-			<Dropdown
-				label="Personality"
-				v-model="personality"
-				:options="personalityOptions"
-				class="spaced"
-				/>
-
-			<Textbox
-				label="Name"
-				v-model="name"
-				class="spaced"
-				/>
-
-			<DynamicForm
-				v-if="addFixtureForm"
-				class="spaced"
-				:form="{ VerticalStack: addFixtureForm[0] }"
-				:formData="addFixtureForm[1]"
-				/>
-			
-			<template #footer>
-				<Button @click="cancelAddingFixture()" subtle class="spaced">
-					Cancel
-				</Button>
-				<div class="spacer"/>
-				<Button @click="addFixture()" class="spaced">
-					Ok
-				</Button>
-			</template>
-		</Dialog>
-		<Dialog
-			:visible="!!creationError"
-			@close-clicked="creationError = null"
-			>
-			<template #header>
-				{{ creationError && creationError.header }}
-			</template>
-			{{ creationError && creationError.body }}
-			<template #footer>
-				<div class="spacer"/>
-				<Button @click="creationError = null" class="spaced">
-					Ok
-				</Button>
-			</template>
-		</Dialog>
+		<CreateFixtureDialog v-model:visible="addFixtureDialog" />
 	</div>
 </template>
 
